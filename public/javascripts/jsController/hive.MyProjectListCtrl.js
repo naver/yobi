39b518f891a1eabb9e64341bdd01d3960c6a18e4
Youngteac Hong <susukang98@gmail.com>
2013-06-17 19:31:28 +0900

    myProjectList: some comments, remove expand tab for naming convention.

--- public/javascripts/jsController/hive.MyProjectListCtrl.js
+++ public/javascripts/jsController/hive.MyProjectListCtrl.js
@@ -1,26 +1,29 @@
 /**
- * MyProjectListCtrl
- */
+* MyProjectListCtrl
+*/
 var MyProjectListCtrl = function($scope, $filter) {
-  $scope.aProject = aProject;
-  $scope.aCreatedProject = $filter('filter')($scope.aProject, {owner: sLoginId});
-  $scope.aBelongedProject = _.difference($scope.aProject, $scope.aCreatedProject);
-  $scope.sPredicate = "";
-  $scope.bReverse = false;
+	// projects
+	$scope.aProject = aProject;
+	$scope.aCreatedProject = $filter('filter')($scope.aProject, {owner: sLoginId});
+	$scope.aBelongedProject = _.difference($scope.aProject, $scope.aCreatedProject);
 
-  /**
-   * orderByDate
-   */
-  $scope.orderByDate = function() {
-    $scope.sPredicate = "createdDate";
-    $scope.bReverse = !$scope.bReverse;
-  };  
+	// orderBy Condition
+	$scope.sPredicate = "";
+	$scope.bReverse = false;
 
-  /**
-   * orderByName
-   */
-  $scope.orderByName = function() {
-    $scope.sPredicate = "name";
-    $scope.bReverse = !$scope.bReverse;
-  };
+	/**
+	* orderByDate
+	*/
+	$scope.orderByDate = function() {
+		$scope.sPredicate = "createdDate";
+		$scope.bReverse = !$scope.bReverse;
+	};  
+
+	/**
+	* orderByName
+	*/
+	$scope.orderByName = function() {
+		$scope.sPredicate = "name";
+		$scope.bReverse = !$scope.bReverse;
+	};
 };
