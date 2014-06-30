14de083197dab5e4626a932b6d22f4095509155f
Changsung Kim <changsung.kim@nhn.com>
2014-06-30 15:38:20 +0900

    Translates comments in `PasswordResetApp.java` into english.

--- app/controllers/PasswordResetApp.java
+++ app/controllers/PasswordResetApp.java
@@ -39,11 +39,26 @@ import static play.data.Form.form;
 
 public class PasswordResetApp extends Controller {
 
+    /**
+     * Moves to the password reset mail sending page.
+     * This method is used when a user clicks the “forgot password” link.
+     *
+     * @return the password reset mail sending page.
+     */
     public static Result lostPassword(){
         // render(message: String, sender: String, errorMessage: String, isSent: Boolean)
         return ok(lostPassword.render("site.resetPasswordEmail.title", null, null, false));
     }
 
+    /**
+     * Requests to send the password reset mail.
+     * This method is used when a user typed in his/her log-in id and email address to request to reset a password.
+     * - Gets {@code loginId}and {@code emailAddress} from the form
+     * - Sends out a mail with the password reset link if the requested {@code loginId} exists and matches {@code email}
+     * - If not, return error message
+     *
+     * @return request for password reset mail      
+     */
     public static Result requestResetPasswordEmail(){
         DynamicForm requestData = form().bindFromRequest();
         String loginId = requestData.get("loginId");
@@ -66,7 +81,19 @@ public class PasswordResetApp extends Controller {
         return ok(lostPassword.render("site.resetPasswordEmail.title", emailAddress, errorMessage, isMailSent));
     }
 
+    /**
+     * Sends out an email that attached a link to the password reset page.
+     * This method is used when it needs to send a password reset email to {@code hashString} generated to reset password.
+     * - This method gets {@code smtp.user} and {@code smtp.domain} from play’s {@code application.conf} and uses it as a sender’s address.
+     * - It attaches the password reset URL to the mail content and sends it.
+     *
+     * @param user  user information found through the email request.
+     * @param hashString  randomized hash string for the given user
+     * @return
+     */
     private static boolean sendPasswordResetMail(User user, String hashString) {
+        // SiteApp.sendMail() should be integrated into this method.
+        // The section for checking site email settings is missing.
         Configuration config = play.Play.application().configuration();
         String sender = config.getString("smtp.user") + "@" + config.getString("smtp.domain");
         String resetPasswordUrl = getResetPasswordUrl(hashString);
@@ -87,6 +114,14 @@ public class PasswordResetApp extends Controller {
         }
     }
 
+    /**
+     * Generates URL link for password reset
+     * - Reads {@code application.hostname} settings and {@code application.port} settings from application.conf.
+     * - If neither has been set up, you should assume that this is under development environment and generate a URL link with LOCAL_HOST_IP:DEV_MODE_PORT
+     *
+     * @param hashString
+     * @return a URL link for password rest
+     */
     private static String getResetPasswordUrl(String hashString) {
         Configuration config = play.Play.application().configuration();
         String hostname = config.getString("application.hostname");
@@ -95,10 +130,21 @@ public class PasswordResetApp extends Controller {
         return "http://" + hostname + "/resetPassword?s=" + hashString;
     }
 
+    /**
+     * Moves to the password reset page
+     */
     public static Result resetPasswordForm(String hashString){
         return ok(resetPassword.render("title.resetPassword", form(User.class), hashString));
     }
 
+    /**
+     * Changes the password of a user who requested to send a password reset email into a new password
+     * - Gets {@code hashString}and {@code password} from the requested form to verify.
+     * - Checks if {@code hashString} is valid.
+     * - If it is valid, this method resets password; otherwise, it writes log and moves to the log-in page.
+     *
+     * @return log-in page
+     */
     public static Result resetPassword(){
         DynamicForm requestData = form().bindFromRequest();
         String hashString = requestData.get("hashString");
